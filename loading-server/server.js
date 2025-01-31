c22b24e2380a09a74ebd07b360ec55abec3a1a7c
dilmurodivcc <zufarovamohira32gmail.com>
2025-01-31 18:01:54 +0500

    change h1

--- loading-server/server.js
+++ loading-server/server.js
@@ -18,5 +18,5 @@ const app = express();
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log("Server is running on... http://localhost:3000");
+  console.log("Server is running on... 3000");
 });
